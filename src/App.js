1b5ef50144e870ecf56e938d1adda198ec8af42c
Your Namei git config --global user.name Your <yousuf8169@gmail.com>
2023-03-19 22:20:05 -0700

    video palyer app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-function App() {
-    return (
-        <div className="App">
-            <div>Hello world</div>
-        </div>
-    );
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+import Video from './pages/Video';
 
+const App = () => {
+  return (
+   
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/video/:videoId' element={<Video />} />
+      </Routes>
+      <Footer/>
+    </Router>
+
+
+  );
+};
 export default App;
